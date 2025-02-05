79f4d2a701cdb20d6d5681bda72a0e73a96fdb79
WalidOne2 <elhamdaoui.walid123@gmail.com>
2025-02-05 22:21:42 +0100

    database: migration réinitialisation mot de passe

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -4,6 +4,9 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+// Migration - Table password_resets
+// Gestion des réinitialisations de mot de passe
+
 return new class extends Migration
 {
     
