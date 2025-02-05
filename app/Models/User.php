2dd43b6ff28c18bd304a2d2701edadd567159a7d
WalidOne2 <elhamdaoui.walid123@gmail.com>
2025-02-05 13:53:30 +0100

    backend: modèle User avec authentification

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,9 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+// Modèle User -Utilisateur
+// Gestion des utilisateurs et authentification
+
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
