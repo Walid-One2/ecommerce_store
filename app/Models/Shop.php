7a4a00088e820175c9e73d595dfc4a6aa174bcdf
WalidOne2 <elhamdaoui.walid123@gmail.com>
2025-02-06 18:30:38 +0100

    backend: modèle Shop pour boutique

--- app/Models/Shop.php
+++ app/Models/Shop.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+// Modèle Shop - Boutique
+// Paramètres et informations de la boutique
+
 class Shop extends Model
 {
     protected $fillable = ['user_id', 'name_shop', 'desc', 'phone', 'address', 'path'];
