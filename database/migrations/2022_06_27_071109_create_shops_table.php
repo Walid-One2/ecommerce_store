3b44f50f3d7389122c4a615d27b0429016b1c6b0
WalidOne2 <elhamdaoui.walid123@gmail.com>
2025-02-06 20:16:50 +0100

    database: migration table shops

--- database/migrations/2022_06_27_071109_create_shops_table.php
+++ database/migrations/2022_06_27_071109_create_shops_table.php
@@ -4,6 +4,9 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+// Migration - Table shops
+// Informations des boutiques
+
 return new class extends Migration
 {
     
