8b693c0a5fbf243cdaa5dd543bf2a533de631018
WalidOne2 <elhamdaoui.walid123@gmail.com>
2025-02-07 16:45:58 +0100

    backend: modèle Product avec relations

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+// Modèle Product - Produit
+// Représente un produit du catalogue
+
 class Product extends Model
 {
     
