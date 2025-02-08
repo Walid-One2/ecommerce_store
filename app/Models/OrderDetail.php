e364aa7eecba7408d1faf4930866e528c26ad73f
WalidOne2 <elhamdaoui.walid123@gmail.com>
2025-02-08 21:16:43 +0100

    backend: modèle OrderDetail

--- app/Models/OrderDetail.php
+++ app/Models/OrderDetail.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+// Modèle OrderDetail - Détail de commande
+// Ligne de commande (produit + quantité)
+
 class OrderDetail extends Model
 {
     protected $fillable = ['order_code', 'title', 'price', 'quantity'];
