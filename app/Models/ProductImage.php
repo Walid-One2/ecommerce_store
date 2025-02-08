919c9da5dd586d4d104b5e7577a9903cca82e522
WalidOne2 <elhamdaoui.walid123@gmail.com>
2025-02-08 09:46:39 +0100

    backend: modèle ProductImage

--- app/Models/ProductImage.php
+++ app/Models/ProductImage.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+// Modèle ProductImage - Image produit
+// Images associées à un produit
+
 class ProductImage extends Model
 {
 
