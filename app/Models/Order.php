6ff88dd6fd8a31227cfd6f875f6172184668d618
WalidOne2 <elhamdaoui.walid123@gmail.com>
2025-02-08 12:45:48 +0100

    backend: modèle Order pour commandes

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+// Modèle Order - Commande
+// Représente une commande client
+
 class Order extends Model
 {
     protected $fillable = ['shop_id', 'order_code', 'name', 'phone', 'address', 'note', 'total', 'status'];
