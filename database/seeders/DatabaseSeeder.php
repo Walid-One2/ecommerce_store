a56d10d410930910abe3f9f4cba94ed1f70cec96
WalidOne2 <elhamdaoui.walid123@gmail.com>
2025-02-09 12:34:54 +0100

    database: seeder données initiales

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+// Seeder - Données initiales
+// Peuple la base de données
+
 class DatabaseSeeder extends Seeder
 {
     
