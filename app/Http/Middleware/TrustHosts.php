67746e433898083a94cbac5d2657f5208ee261ae
WalidOne2 <elhamdaoui.walid123@gmail.com>
2025-02-11 08:08:15 +0100

    middleware: hôtes de confiance

--- app/Http/Middleware/TrustHosts.php
+++ app/Http/Middleware/TrustHosts.php
@@ -4,6 +4,9 @@ namespace App\Http\Middleware;
 
 use Illuminate\Http\Middleware\TrustHosts as Middleware;
 
+// Middleware - Hôtes de confiance
+// Validation des hôtes autorisés
+
 class TrustHosts extends Middleware
 {
     
