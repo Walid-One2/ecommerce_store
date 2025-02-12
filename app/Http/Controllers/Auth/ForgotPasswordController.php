01e0f6c0b388988910dde086d5fd0024f58b2233
WalidOne2 <elhamdaoui.walid123@gmail.com>
2025-02-12 12:42:48 +0100

    auth: contrôleur mot de passe oublié

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
 
+// Contrôleur Auth - Mot de passe oublié
+// Gestion de la réinitialisation du mot de passe
+
 class ForgotPasswordController extends Controller
 {
     
