522ac98d200f14e436fc5cf3b5decb2cb6fd2b2d
WalidOne2 <elhamdaoui.walid123@gmail.com>
2025-02-12 19:44:41 +0100

    auth: contrôleur réinitialisation mot de passe

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 
+// Contrôleur Auth - Réinitialisation mot de passe
+// Traitement du nouveau mot de passe
+
 class ResetPasswordController extends Controller
 {
     
