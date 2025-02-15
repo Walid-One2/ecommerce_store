8e1c788b5ecde24771622793e7e61ebd29c9624c
WalidOne2 <elhamdaoui.walid123@gmail.com>
2025-02-15 09:06:04 +0100

    routes: endpoints API REST

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// Routes API - Endpoints REST
+// Routes pour l'API
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
