23d63b1e17c9cd8bf75dd95d78536d5e84582045
WalidOne2 <elhamdaoui.walid123@gmail.com>
2025-02-15 12:06:59 +0100

    routes: commandes Artisan

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,9 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
+// Routes - Commandes Artisan
+// Commandes en ligne de commande
+
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
