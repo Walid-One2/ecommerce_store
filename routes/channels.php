f0c72b8f3029c451c8c32fe00c134c737e653bcd
WalidOne2 <elhamdaoui.walid123@gmail.com>
2025-02-15 10:35:02 +0100

    routes: canaux broadcasting

--- routes/channels.php
+++ routes/channels.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Broadcast;
 
+// Routes - Canaux Broadcasting
+// Canaux de diffusion temps réel
+
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
