bdc545518a8d0f2b9d7033e2dfa099da4834a481
WalidOne2 <elhamdaoui.walid123@gmail.com>
2025-02-20 15:05:40 +0000

    client: fiche produit

--- resources/views/client/productDetail.blade.php
+++ resources/views/client/productDetail.blade.php
@@ -1,3 +1,4 @@
+{-- Composant Blade - productDetail --}
 <x-template.layout title="{{ $title }}" >
   <x-organisms.navbar cartCount=10 :path="$shop->path"/>
     <div class="container pt-md-5 pt-3">
