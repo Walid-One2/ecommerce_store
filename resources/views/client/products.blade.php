060679a01736b01a7291b06acd90add311840e0d
WalidOne2 <elhamdaoui.walid123@gmail.com>
2025-02-20 12:21:40 +0000

    client: catalogue produits

--- resources/views/client/products.blade.php
+++ resources/views/client/products.blade.php
@@ -1,3 +1,4 @@
+{-- Composant Blade - products --}
 <x-template.layout title="{{$title}}">
   <x-organisms.navbar cartCount=10 :path="$shop->path"/>
   <x-organisms.carousel-category :category="$category" />
