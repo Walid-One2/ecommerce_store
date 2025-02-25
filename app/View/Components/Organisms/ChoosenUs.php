13abdd4b664208d84e46325d77bc97cf57bd00bd
WalidOne2 <elhamdaoui.walid123@gmail.com>
2025-02-25 08:19:42 +0000

    component: organism pourquoi nous choisir

--- app/View/Components/Organisms/ChoosenUs.php
+++ app/View/Components/Organisms/ChoosenUs.php
@@ -4,6 +4,9 @@ namespace App\View\Components\Organisms;
 
 use Illuminate\View\Component;
 
+// Composant Organism - Pourquoi nous choisir
+// Section arguments de vente
+
 class ChoosenUs extends Component
 {
     
