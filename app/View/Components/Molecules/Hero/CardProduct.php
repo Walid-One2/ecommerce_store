7673846a365f66f16a52fbfaa24a9d784f5a4ccb
WalidOne2 <elhamdaoui.walid123@gmail.com>
2025-02-27 16:34:52 +0000

    component: molecule carte produit héro

--- app/View/Components/Molecules/Hero/CardProduct.php
+++ app/View/Components/Molecules/Hero/CardProduct.php
@@ -4,6 +4,9 @@ namespace App\View\Components\Molecules\Hero;
 
 use Illuminate\View\Component;
 
+// Composant Molecule Hero - Carte produit héro
+// Produit mis en avant dans le héro
+
 class CardProduct extends Component
 {
     
