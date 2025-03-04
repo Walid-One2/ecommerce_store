cdf5338a987d725389677ba6b903b240dfca66c3
WalidOne2 <elhamdaoui.walid123@gmail.com>
2025-03-04 10:46:35 +0000

    view: molecule bouton réutilisable

--- resources/views/client/components/molecules/button.blade.php
+++ resources/views/client/components/molecules/button.blade.php
@@ -1,3 +1,4 @@
+{-- Composant Blade - button --}
 <div class="text-{{ $align }}">
     <a href="{{ $link }}" class="btn btn-{{$size}} btn-{{ $type }} rounded-pill fw-bold p-md-3 p-2 px-3">
         <div class="d-flex align-items-center gap-3">
