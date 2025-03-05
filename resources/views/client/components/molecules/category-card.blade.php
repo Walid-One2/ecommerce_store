f319392a07bb42d008cecf6f50f12fea79e889bf
WalidOne2 <elhamdaoui.walid123@gmail.com>
2025-03-05 08:06:43 +0000

    view: molecule carte catégorie

--- resources/views/client/components/molecules/category-card.blade.php
+++ resources/views/client/components/molecules/category-card.blade.php
@@ -1,3 +1,4 @@
+{-- Composant Blade - category-card --}
 <div class="category-card" style="width:{{$width}};background-image: url({{ asset('shop/products/'. $path ) }})">
     <div class="py-5 ">
         <h2 class="text-center text-white">{!! str_replace('-', ' ', ucwords($name)) !!}</h2>
